b7755b5f9649cc476ab35a9b2c78886c6ea618af
sultan <sultankurbanov302@gmail.com>
2026-06-13 22:38:43 +0500

    app configs

---
+++ src/config/config.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+
+class AppConfig(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=f"{BASE_DIR}/.env",
+    )
+    DATABASE_URL: str | None = None
+    REDIS_URL: str | None = None
+
+
+def get_app_config() -> AppConfig:
+    return AppConfig()
+
+settings = get_app_config()
+
