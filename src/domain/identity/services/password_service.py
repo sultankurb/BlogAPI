28d3a5bd9057681ed3ad6d68a8ab44b209deb1bb
sultan <sultankurbanov302@gmail.com>
2026-06-21 19:05:23 +0500

    auth dependencies services

---
+++ src/domain/identity/services/password_service.py
@@ -0,0 +1,21 @@
+import bcrypt
+
+
+class PasswordService:
+    @staticmethod
+    def hash_password(password: str) -> str:
+        password_byte = password.encode()
+        salt= bcrypt.gensalt()
+        return bcrypt.hashpw(password=password_byte, salt=salt).decode()
+    @staticmethod
+    def verify_password(password: str, hashed_password: str) -> bool:
+        hashed_password_byte = hashed_password.encode()
+        password_byte = password.encode()
+        return bcrypt.checkpw(
+            password=password_byte,
+            hashed_password=hashed_password_byte
+        )
+
+
+def get_password_service() -> PasswordService:
+    return PasswordService()
