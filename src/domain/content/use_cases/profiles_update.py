839f08ddaa7de6a30adef0e500edc4fec13420aa
sultan <sultankurbanov302@gmail.com>
2026-06-28 13:52:06 +0500

    first steps of make it full functional

---
+++ src/domain/content/use_cases/profiles_update.py
@@ -0,0 +1,24 @@
+from src.domain.content.schemas import ProfileReadModel, ProfileUpdatedModel
+from src.infrastructure.database import UnitOfWork
+
+
+class ProfilesUpdateUseCase:
+    def __init__(self, uow: UnitOfWork):
+        self._uow = uow
+
+    async def execute(
+            self,
+            pk: int,
+            data: ProfileUpdatedModel,
+            partial: bool = False
+    ) -> ProfileReadModel:
+        data.model_dump(exclude_unset=partial)
+        async with self._uow as uow:
+            result = await uow.update_profile(pk=pk, data=data)
+
+        return ProfileReadModel(
+            first_name=result.first_name,
+            last_name=result.last_name,
+            biograph=result.biograph,
+            username=result.username
+        )
